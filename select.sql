5eed09918e65b74997ca9bd2a67ea55084444e70
cavignesh <120577397+cavignesh@users.noreply.github.com>
2024-06-25 11:33:28 +0530

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,1 +1,34 @@
-DROP TABLE customer_demographics CASCADE;
+-- FUNCTION: public.ActivateElement(integer)
+
+-- DROP FUNCTION IF EXISTS public."ActivateElement"(integer);
+
+CREATE OR REPLACE FUNCTION public."ActivateElement"(
+	in_element_id integer)
+    RETURNS boolean
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+-- It will make the active_flag for the given element as true
+BEGIN
+	UPDATE m_element SET active_flag = TRUE WHERE id = in_element_id;
+	IF FOUND THEN return TRUE; END IF;
+	return FALSE;
+END;
+$BODY$;
+
+ALTER FUNCTION public."ActivateElement"(integer)
+    OWNER TO root;
+
+GRANT EXECUTE ON FUNCTION public."ActivateElement"(integer) TO PUBLIC;
+
+GRANT EXECUTE ON FUNCTION public."ActivateElement"(integer) TO gis_user;
+
+GRANT EXECUTE ON FUNCTION public."ActivateElement"(integer) TO lambda_user;
+
+GRANT EXECUTE ON FUNCTION public."ActivateElement"(integer) TO node_user;
+
+GRANT EXECUTE ON FUNCTION public."ActivateElement"(integer) TO race_user;
+
+GRANT EXECUTE ON FUNCTION public."ActivateElement"(integer) TO root;
+
